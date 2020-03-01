4671a752168ded61646e6af989770622ed5216f6
allalaspace (Boudiche) <allalaspace@gmail.com>
2020-03-01 22:41:08 +0100

    add Controller

---
+++ Models/DCandidate.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApi.Models
+{
+    public class DCandidate
+    {
+        [Key]
+        public int Id { get; set; }
+        [Column(TypeName ="nvarchar(100)")]
+        public int fullName { get; set; }
+        [Column(TypeName = "nvarchar(100)")]
+        public int mobile { get; set; }
+        [Column(TypeName = "nvarchar(100)")]
+        public int email { get; set; }
+        [Column]
+        public int age { get; set; }
+        [Column(TypeName = "nvarchar(3)")]
+        public int bloodGroup { get; set; }
+        
+        [Column(TypeName = "nvarchar(100)")]
+        public int adresse { get; set; }
+
+
+
+
+
+    }
+}
--- Models/DonationDBContext.cs
+++ Models/DonationDBContext.cs
@@ -1,11 +1,18 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace WebApi.Models
 {
-    public class DonationDBContext
+    public class DonationDBContext : DbContext
     {
+
+        public DonationDBContext(DbContextOptions<DonationDBContext> options):base(options)
+        {
+
+        }
+        public DbSet<DCandidate> DCandidates { get; set; }
     }
 }
